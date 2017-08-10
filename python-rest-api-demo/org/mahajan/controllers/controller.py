1fb80033d8349b1b6ccd6b04dd9fe8bcf995f401
Neeraj Mahajan <neeraj.mahajan@specsavers.com>
2017-08-10 10:31:08 +0100

    Added the project structure for rest api demo

---
+++ python-rest-api-demo/org/mahajan/controllers/controller.py
@@ -0,0 +1,17 @@
+'''
+Created on 10 Aug 2017
+
+@author: neeraj.mahajan
+'''
+
+class CustomerController (object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ python-rest-api-demo/org/mahajan/entities/entity.py
@@ -0,0 +1,21 @@
+'''
+Created on 10 Aug 2017
+
+@author: neeraj.mahajan
+'''
+
+class Customer (object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, id,firstName,lastName,dob):
+        '''
+        Constructor
+        '''
+        self.id=id
+        self.firstName=firstName
+        self.lastName=lastName
+        self.dob=dob
+        
